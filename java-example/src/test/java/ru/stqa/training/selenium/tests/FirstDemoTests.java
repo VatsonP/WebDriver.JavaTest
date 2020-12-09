0dc6a67c2189bf345b047b3924c75093ab1b7822
VatsonP <58036174+VatsonP@users.noreply.github.com>
2020-12-09 11:32:39 +0200

    Separate proc calls

--- java-example/src/test/java/ru/stqa/training/selenium/tests/FirstDemoTests.java
+++ java-example/src/test/java/ru/stqa/training/selenium/tests/FirstDemoTests.java
@@ -1,17 +1,11 @@
 package ru.stqa.training.selenium.tests;
 
 import org.junit.*;
-import ru.stqa.training.selenium.driverbase.DriverBase;
+
 import ru.stqa.training.selenium.driverbase.DriverBaseParams;
 import ru.stqa.training.selenium.testapp.FirstDemoApp;
 import ru.stqa.training.selenium.testbase.TestBase;
 
-import java.lang.Thread;
-import java.util.List;
-
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 
 public class FirstDemoTests extends TestBase {
@@ -35,18 +29,43 @@ public class FirstDemoTests extends TestBase {
 
 
     @Test
-    public void myFirstDemo() {
+    public void my1stFirstDemo() {
         getApp().initPages(getDrvBase());
 
         getApp().FirstTest();
+    }
+
+    @Test
+    public void my2ndTest() {
+        getApp().initPages(getDrvBase());
 
         getApp().SecondTest();
+    }
+
+    @Test
+    public void my3rdTest() {
+        getApp().initPages(getDrvBase());
 
         getApp().ThirdTest();
+    }
+
+    @Test
+    public void my4thTest() {
+        getApp().initPages(getDrvBase());
 
         getApp().FourthTest();
+    }
+
+    @Test
+    public void my5thTest() {
+        getApp().initPages(getDrvBase());
 
         getApp().FifthTest();
+    }
+
+    @Test
+    public void my6thTest() {
+        getApp().initPages(getDrvBase());
 
         getApp().SixthTest_myCheckStiker();
     }
